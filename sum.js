405e6ff295b40f7d34392463602db54c6d329131
Ana Claudia <anacnogueira@gmail.com>
2023-04-23 19:44:31 -0300

    Adding tests to Sum Test

--- sum.js
+++ sum.js
@@ -1,5 +1,12 @@
 function sum(a, b) {
-    return a + b;
+    const int1 = parseInt(a,10);
+    const int2 = parseInt(b,10);
+
+    if (Number.isNaN(int1) || Number.isNaN(int2)) {
+        throw new Error('Please check your input');
+    }
+
+    return  int1 + int2;
 }
 
 module.exports = sum;
--- sum.test.js
+++ sum.test.js
@@ -3,3 +3,29 @@ const sum = require('./sum');
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1,2)).toBe(3);
 });
+
+test('should sum 2 and 2 and the result must 4', () => {
+    expect(sum(2,2)).toBe(4);
+});
+
+test('should sum 2 and 2 even if one them is a string and the result must be 4', () => {
+    expect(sum('2','2')).toBe(4);
+});
+
+test('should throw an error if what is provided to the method cannot be sumed', () => {
+    expect(() => {
+        sum('','2')
+    }).toThrowError();
+
+    expect(() => {
+        sum([2,2])
+    }).toThrowError();
+
+    expect(() => {
+        sum({})
+    }).toThrowError();
+
+    expect(() => {
+        sum()
+    }).toThrowError();
+});

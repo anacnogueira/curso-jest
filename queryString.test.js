6a500c80204c029e9b178f3eb4be3fffc934ca89
Ana Claudia <anacnogueira@gmail.com>
2023-05-01 11:49:11 -0300

    More cases of test of QueryStrin

--- queryString.test.js
+++ queryString.test.js
@@ -1,13 +1,36 @@
-const { queryString } = require("./queryString");
+const { queryString } = require('./queryString');
 
-describe("Object to query string", () => {
-  test("should create a valid query string when an object is provided", () => {
+describe('Object to query string', () => {
+  test('should create a valid query string when an object is provided', () => {
     const obj = {
-      name: "Ana",
-      profession: "developer",
+      name: 'Ana',
+      profession: 'developer',
     };
 
-    expect(queryString(obj)).toBe("name=Ana&profession=developer");
+    expect(queryString(obj)).toBe('name=Ana&profession=developer');
+  });
+
+  test('should create a valid querty string even when an array passed as value', () => {
+    const obj = {
+      name: 'Ana',
+      abilities: ['JS', 'TDD'],
+    };
+
+    expect(queryString(obj)).toBe('name=Ana&abilities=JS,TDD');
+  });
+
+  test('should throw an error when an object is passed as value', () => {
+    const obj = {
+      name: 'Ana',
+      abilities: {
+        first: 'JS',
+        second: 'TDD',
+      },
+    };
+
+    expect(() => {
+      queryString(obj);
+    }).toThrowError();
   });
 });
 

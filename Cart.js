8cb29270c7b6a25b0e2baf473dfac357e001a378
Ana Claudia <anacnogueira@gmail.com>
2023-05-11 11:27:18 -0300

    test adding products to cart

--- Cart.js
+++ Cart.js
@@ -1,5 +1,13 @@
 export default class Cart {
+  items = [];
+
+  add(item) {
+    this.items.push(item);
+  }
+
   getTotal() {
-    return 0;
+    return this.items.reduce((acc, item) => {
+      return acc + item.quantity * item.product.price;
+    }, 0);
   }
 }
--- Cart.test.js
+++ Cart.test.js
@@ -1,8 +1,25 @@
 import Cart from './Cart';
+let cart;
+
+beforeEach(() => {
+  cart = new Cart();
+});
 
 describe('Cart', () => {
-  test('should return 0 when getTotal() is executed in a newly created instance ', () => {
-    const cart = new Cart();
+  test('should return 0 when getTotal() is executed in a newly created instance', () => {
     expect(cart.getTotal()).toEqual(0);
+  });
+
+  test('should multiply quantity and price and receive the total amount', () => {
+    const item = {
+      product: {
+        title: 'Adidas Running Shoes Pink - Women',
+        price: 35388, // 353.88 | R$ 383,88,
+      },
+      quantity: 2, //70776
+    };
+
+    cart.add(item);
+    expect(cart.getTotal()).toEqual(70776);
   });
 });

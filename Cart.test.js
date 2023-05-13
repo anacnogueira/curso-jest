ac8e74447531c2739a0d5257d7cbd126027523bc
Ana Claudia <anacnogueira@gmail.com>
2023-05-13 16:22:46 -0300

    test adding same product and reoving product

--- Cart.test.js
+++ Cart.test.js
@@ -1,5 +1,14 @@
 import Cart from './Cart';
 let cart;
+let product = {
+  title: 'Adidas Running Shoes Pink - Women',
+  price: 35388, // 353.88 | R$ 383,88,
+};
+
+let product2 = {
+  title: 'Adidas Running Shoes Pink - men',
+  price: 41872,
+};
 
 beforeEach(() => {
   cart = new Cart();
@@ -12,14 +21,41 @@ describe('Cart', () => {
 
   test('should multiply quantity and price and receive the total amount', () => {
     const item = {
-      product: {
-        title: 'Adidas Running Shoes Pink - Women',
-        price: 35388, // 353.88 | R$ 383,88,
-      },
+      product,
       quantity: 2, //70776
     };
 
     cart.add(item);
     expect(cart.getTotal()).toEqual(70776);
+  });
+
+  test('should ensure no more than on product exists at a time', () => {
+    cart.add({
+      product,
+      quantity: 1,
+    });
+
+    cart.add({
+      product,
+      quantity: 2,
+    });
+
+    expect(cart.getTotal()).toEqual(70776);
+  });
+
+  test('should update total when a product gets included and then removed ', () => {
+    cart.add({
+      product,
+      quantity: 2,
+    });
+
+    cart.add({
+      product: product2,
+      quantity: 1,
+    });
+
+    cart.remove(product);
+
+    expect(cart.getTotal()).toEqual(41872);
   });
 });

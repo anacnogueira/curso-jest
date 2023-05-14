221b658c335141f157ec763d164755e93bd4944b
Ana Claudia <anacnogueira@gmail.com>
2023-05-14 12:11:18 -0300

    Apply percentage discount to  amount

--- Cart.test.js
+++ Cart.test.js
@@ -102,4 +102,21 @@ describe('Cart', () => {
       expect(cart.getTotal().getAmount()).toEqual(0);
     });
   });
+
+  describe('special conditions', () => {
+    test('should apply percentage discount above minimum is passed', () => {
+      const condition = {
+        percentage: 30,
+        minimum: 2,
+      };
+
+      cart.add({
+        product,
+        condition,
+        quantity: 3,
+      });
+
+      expect(cart.getTotal().getAmount()).toEqual(74315);
+    });
+  });
 });

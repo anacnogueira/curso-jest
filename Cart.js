52c7e6fabe4fd7f285584db44b3f2582cbd7b64b
Ana Claudia <anacnogueira@gmail.com>
2023-05-14 18:17:24 -0300

    Extract discount in a new file

--- Cart.js
+++ Cart.js
@@ -1,46 +1,7 @@
 import find from 'lodash/find';
 import remove from 'lodash/remove';
 import Dinero from 'dinero.js';
-
-const calculatePercentageDiscount = (amount, { condition, quantity }) => {
-  if (condition?.percentage && quantity > condition.minimum) {
-    return amount.percentage(condition.percentage);
-  }
-
-  return Money({ amount: 0 });
-};
-
-const calculateQuantityDiscount = (amount, { condition, quantity }) => {
-  const isEven = quantity % 2 === 0;
-
-  if (condition?.quantity && quantity > condition.quantity) {
-    return amount.percentage(isEven ? 50 : 40);
-  }
-
-  return Money({ amount: 0 });
-};
-
-const calculateDiscount = (amount, quantity, condition) => {
-  const list = Array.isArray(condition) ? condition : [condition];
-
-  const [higherDiscount] = list
-    .map(cond => {
-      if (cond.percentage) {
-        return calculatePercentageDiscount(amount, {
-          condition: cond,
-          quantity,
-        }).getAmount();
-      } else if (cond.quantity) {
-        return calculateQuantityDiscount(amount, {
-          condition: cond,
-          quantity,
-        }).getAmount();
-      }
-    })
-    .sort((a, b) => b - a);
-
-  return Money({ amount: higherDiscount });
-};
+import { calculateDiscount } from './discount.utils';
 
 const Money = Dinero;
 Money.defaultCurrency = 'BRL';
